52523b142937cf14747780b20dc892e91c822f51
Hanut Kumar Arora <36478891+HanutArora@users.noreply.github.com>
2019-02-02 01:23:34 +0530

    Update Q1.c

--- Questions/1.0.1/Q1.c
+++ Questions/1.0.1/Q1.c
@@ -1,25 +1,34 @@
 #include<stdio.h>
 #include<conio.h>
-void uniqPrint(int *arr, int lenArr);
+void uniqPrint(int *arr, int lenArr);     //  declaration of function to print only unique elements of an array
 main()
 {
-  int i;
-  int lenArray;
+  int i;                                  //  for loops
+  int lenArray;                           //  for storing the length of the array / total number of elements
   printf("Enter number of elements: ");
   scanf("%d", &lenArray);
-  int Arr[lenArray];
-  for (i = 0; i < lenArray; i++)
+  int Arr[lenArray];                      //  creating array for storing the elements
+  for (i = 0; i < lenArray; i++)          //  populating the array
   {
-    printf("Enter %d element: ");
-    scanf("%d", &Arr[i];
+    printf("Enter %d element: ", i+1);
+    scanf("%d", &Arr[i]);
   }
-  uniqPrint(Arr, lenArray);
+  uniqPrint(Arr, lenArray);               //  calling function to print unique elements
 }
 void uniqPrint(int *arr, int lenArr)
 {
-  int i;
-  for (i = 0; i < lenArrl; i++)
+  int i, j;         //  for loops
+  int flag = 0;     //  for checking if the element is duplicate or not
+  printf("\nUnique Elements:\t%d\t", arr[0]);    //  starting the list of unique elements alongwith the first element
+  for (i = 1; i < lenArr; i++)      //  for printing unique elements
   {
-    
+    for (j = i-1; j>=0; j--)        //  loop for checking if the element is duplicate
+    {
+      if (arr[i] == arr[j])
+        flag = 1;
+    }
+    if (flag == 0)                  //  printing only unique elements
+    	printf("%d\t", arr[i]);
+    flag = 0;
   }
 }
