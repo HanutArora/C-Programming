ef27b53eed44fa4ba8c6792f758126d01fa850c1
Hanut Kumar Arora <36478891+HanutArora@users.noreply.github.com>
2019-02-02 01:53:15 +0530

    Update Q2.c
    Commenting

--- Questions/1.0.1/Q2.c
+++ Questions/1.0.1/Q2.c
@@ -2,17 +2,17 @@
 #include<conio.h>
 void addNum(int *n1, int *n2)
 {
-  *n1 += *n2;
+  *n1 += *n2;                                   //  calculating the sum i.e., adding 2nd element to the first element
 }
 main()
 {
-  int num, num2;
-  int *ptr1 = &num, *ptr2 = &num2;
+  int num, num2;                                //  variables to store the input
+  int *ptr1 = &num, *ptr2 = &num2;              //  pointers for respective variables
   printf("Enter first number: ");
   scanf("%d", ptr1);
   printf("Enter second number: ");
   scanf("%d", ptr2);
-  addNum(ptr1, ptr2);
-  printf("Sum of the 2 numbers = %d", *ptr1);
+  addNum(ptr1, ptr2);                           //  passing pointers to the function
+  printf("Sum of the 2 numbers = %d", *ptr1);   //  printing the sum of the variables
   getch();
 }
