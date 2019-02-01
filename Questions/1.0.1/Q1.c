6c8bb5a19759ee9c7261ead6c1d412939b815d3b
Hanut Kumar Arora <36478891+HanutArora@users.noreply.github.com>
2019-02-01 14:36:22 +0530

    Create Q1.c

---
+++ Questions/1.0.1/Q1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void uniqPrint(int *arr, int lenArr);
+main()
+{
+  int i;
+  int lenArray;
+  printf("Enter number of elements: ");
+  scanf("%d", &lenArray);
+  int Arr[lenArray];
+  for (i = 0; i < lenArray; i++)
+  {
+    printf("Enter %d element: ");
+    scanf("%d", &Arr[i];
+  }
+  uniqPrint(Arr, lenArray);
+}
+void uniqPrint(int *arr, int lenArr)
+{
+  int i;
+  for (i = 0; i < lenArrl; i++)
+  {
+    
+  }
+}
